ba7bd71b36bfd2fd7623540b58d9a79d8771fb5c
g1eb <mail@g1eb.com>
2017-10-26 19:48:31 +0000

    Added a helper function to convert number of seconds to a human readable time format.

--- src/calendar-heatmap.component.js
+++ src/calendar-heatmap.component.js
@@ -1629,6 +1629,27 @@ class CalendarHeatmap extends React.Component {
   }
 
 
+  /**
+   * Helper function to convert seconds to a human readable format
+   * @param seconds Integer
+   */
+  formatTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    var time = '';
+    if (hours > 0) {
+      time += hours === 1 ? '1 hour ' : hours + ' hours ';
+    }
+    if (minutes > 0) {
+      time += minutes === 1 ? '1 minute' : minutes + ' minutes';
+    }
+    if (hours === 0 && minutes === 0) {
+      time = Math.round(seconds) + ' seconds';
+    }
+    return time;
+  }
+
+
   render() {
     return (
       <div id="calendar-heatmap"></div>
