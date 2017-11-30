3948262128b4b493b467285e74982c6b2b4a2b8e
g1eb <mail@g1eb.com>
2017-11-30 14:16:23 +0100

    Fix import statement for react component

--- src/calendar-heatmap.component.js
+++ src/calendar-heatmap.component.js
@@ -1,9 +1,9 @@
-import { Component } from 'react'
+import * as React from 'react'
 
 import moment from 'moment'
 import * as d3 from 'd3'
 
-class CalendarHeatmap extends Component {
+class CalendarHeatmap extends React.Component {
 
   constructor(props) {
     super(props)
