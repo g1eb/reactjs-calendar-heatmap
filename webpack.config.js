7c7edb1da624df752e62ac7ed9f237070d716eaa
g1eb <mail@g1eb.com>
2017-12-08 11:58:22 +0100

    Add css-modules to the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
-        }
+        },
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
       }
     ]
   }
