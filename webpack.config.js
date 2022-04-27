fd1c5457ef95fae48400344194af19dd2ce4cb4f
maneetgoyal <maneetgoyal14@gmail.com>
2022-04-27 18:58:17 +0530

    Add babel-loader runtime: atuomatic (// Ref: https://stackoverflow.com/questions/32070303/uncaught-referenceerror-react-is-not-defined])

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }], // Ref: https://stackoverflow.com/questions/32070303/uncaught-referenceerror-react-is-not-defined]
+            ],
           },
         },
       },
