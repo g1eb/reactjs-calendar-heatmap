f02f4c9de8f5558313b42d85abecd9b947dd62c9
Qurban <ahmad76qurban@gmail.com>
2022-04-28 09:44:56 +0530

    build: remove controls from preview.ks

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,3 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
 };
