914ffebd854e3278b17668c51a8d835e84e72690
Qurban <ahmad76qurban@gmail.com>
2022-04-30 15:19:20 +0530

    refactor: refactor summary logic

--- src/calendar-heatmap.component.js
+++ src/calendar-heatmap.component.js
@@ -159,19 +159,21 @@ export class CalendarHeatmap extends Component {
     );
 
     const getSummary = (date) => {
+      // filtering the 'data' based on the year of date and extracting all the summaries from the data into a single dimensional array
+      const summaries = this.props.data
+        .filter(
+          (e) => new Date(e.date).getFullYear() === new Date(date).getFullYear()
+        )
+        .flatMap((e) => e.summary);
       // Create 'summary' dictionary: Record<string, {name: string; value: number}>
-      let summaryDictionary = this.props.data.reduce((summary, d) => {
-        if (moment(d.date).year() === date.year()) {
-          d.summary.map((item) => {
-            if (summary[item.name] === undefined) {
-              summary[item.name] = {
-                name: item.name,
-                value: item.value,
-              };
-            } else {
-              summary[item.name].value += item.value;
-            }
-          });
+      let summaryDictionary = summaries.reduce((summary, item) => {
+        if (summary[item.name] === undefined) {
+          summary[item.name] = {
+            name: item.name,
+            value: item.value,
+          };
+        } else {
+          summary[item.name].value += item.value;
         }
         return summary;
       }, {});
@@ -191,7 +193,7 @@ export class CalendarHeatmap extends Component {
           }
           return prev;
         }, 0),
-        summary: getSummary(date),
+        summary: getSummary(d),
       };
     });
 
