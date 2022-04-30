5fca2d089e8ee575f14361d3a915c7bf9930f6b9
Qurban <ahmad76qurban@gmail.com>
2022-04-30 16:58:51 +0530

    refactor: break function into smaller ones

--- src/utils.js
+++ src/utils.js
@@ -32,31 +32,40 @@ export function createColorGenerator(min_value, max_value, color) {
   return colorGenerator;
 }
 
+// Create 'summary' dictionary: Record<string, {name: string; value: number}>
+function createSummaryDictionary(summariesOrDetails) {
+  return summariesOrDetails.reduce((summary, item) => {
+    if (summary[item.name] === undefined) {
+      summary[item.name] = {
+        name: item.name,
+        value: item.value,
+      };
+    } else {
+      summary[item.name].value += item.value;
+    }
+    return summary;
+  }, {});
+}
+
+// Sort summary dictionary
+function sortSummaryDictionary(summaryDictionary) {
+  return Object.values(summaryDictionary).sort((a, b) => {
+    return b.value - a.value;
+  });
+}
+
 export function getYearSummary(data, date) {
   /**
    * Filtering the 'data' based on the year of date,
    * extracting all the summaries from the data into a single dimensional array and
-   * create 'summary' dictionary: Record<string, {name: string; value: number}>
    */
-  const summaryDictionary = data
+  const summaries = data
     .filter(
       (e) => new Date(e.date).getFullYear() === new Date(date).getFullYear()
     )
-    .flatMap((e) => e.summary)
-    .reduce((summary, item) => {
-      if (summary[item.name] === undefined) {
-        summary[item.name] = {
-          name: item.name,
-          value: item.value,
-        };
-      } else {
-        summary[item.name].value += item.value;
-      }
-      return summary;
-    }, {});
-  return Object.values(summaryDictionary).sort((a, b) => {
-    return b.value - a.value;
-  });
+    .flatMap((e) => e.summary);
+  const summaryDictionary = createSummaryDictionary(summaries);
+  return sortSummaryDictionary(summaryDictionary);
 }
 
 // Calculate daily summary if that was not provided
@@ -65,21 +74,9 @@ export function parseData(data) {
     if (data[0].summary === null || data[0].summary === undefined) {
       data.forEach((d) => {
         // Create project dictionary: Record<string, {name: string; value: number}>
-        let summaryDictionary = d.details.reduce((uniques, project) => {
-          if (uniques[project.name] === undefined) {
-            uniques[project.name] = {
-              name: project.name,
-              value: project.value,
-            };
-          } else {
-            uniques[project.name].value += project.value;
-          }
-          return uniques;
-        }, {});
+        const summaryDictionary = createSummaryDictionary(d.details);
         // Update "summary" property of the array element
-        d.summary = Object.values(summaryDictionary).sort((a, b) => {
-          return b.value - a.value;
-        });
+        d.summary = sortSummaryDictionary(summaryDictionary);
       });
     }
   }
