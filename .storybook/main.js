67933caced9b94bb7f9bdb33a0e05f9eef4098f1
maneetgoyal <mgoyal@manufacanalytics.com>
2022-05-24 15:30:54 +0530

    Update deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
   ],
-  framework: '@storybook/react',
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  },
   core: {
     builder: 'webpack5',
   },
+  framework: '@storybook/react',
+  features: { storyStoreV7: true, buildStoriesJson: true }, // Ref: https://storybook.js.org/docs/react/configure/overview#feature-flags
 };
